f04c417c546bd2b0f2219452cec154ac0b3e20d7
Gaurav khurana <gauravkhurana154@gmail.com>
2023-06-18 16:20:32 +0530

    fix

--- src/ChatPage/ChatBot/index.js
+++ src/ChatPage/ChatBot/index.js
@@ -9,7 +9,7 @@ import User from "../../assets/User.png";
 const contentStyle = {
   fontSize: "16px",
   padding: "10px",
-  height: "68vh",
+  height: "40vh",
   //   width: "100%",
   scrollbarWidth: 0,
   "-ms-overflow-style": "none",
